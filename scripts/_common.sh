43ba2bf60ec0612743e90d3ae2b280f017a1a9d2
Bruno Pinna <brunopinna87@gmail.com>
2024-12-27 17:56:09 +0100

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -3,6 +3,8 @@
 #=================================================
 # COMMON VARIABLES
 #=================================================
+# PHP version required by FIII Data Importer
+YNH_PHP_VERSION="8.3"
 
 # Composer version
 YNH_COMPOSER_VERSION="2.5.4"
